86deb92101d4e2e4b7895e2c0226c462851dffa8
Caleb Lagge <caleb.i.lagge-1@ou.edu>
2019-04-18 09:42:44 -0500

    add todo

---
+++ src/Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-16
+OU ID_______________: 113478036
+OU EMAIL____________: caleb.i.lagge-1@ou.edu
+
+Tasks:
+1. make the test
+2. make the calculate twoTokensMethod
+3. make the calculate threeTokensMethod
+4. make the execute method
+5. make the parseAndExecute method
+
+Estimated Time (minutes):
+1. 60
+2. 20
+3. 20
+4. 30
+5. 30
+
+Actual Time (minutes):
+1. 90
+2. 10
+3. 30
+4. 30
+5. 30
+*/}
