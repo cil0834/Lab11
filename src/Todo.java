27f645a8a7059e053ec577762f27ef62199917a4
Caleb Lagge <caleb.i.lagge-1@ou.edu>
2019-04-21 23:18:39 -0500

    updated todo

--- src/Todo.java
+++ src/Todo.java
@@ -1,27 +1,30 @@
 public class Todo {/*
 Lab Section_________: 11
-Last Modified_______: 2019-02-16
+Last Modified_______: 2019-04-21
 OU ID_______________: 113478036
 OU EMAIL____________: caleb.i.lagge-1@ou.edu
 
 Tasks:
-1. make the test
-2. make the calculate twoTokensMethod
-3. make the calculate threeTokensMethod
-4. make the execute method
-5. make the parseAndExecute method
+1. setUpRegions method
+2. paintComponent method
+3. mouseClicked method
+4. setSelectedRegionContents method
+5. evaluate method
+6. the constructor
 
 Estimated Time (minutes):
-1. 60
+1. 15
 2. 20
-3. 20
-4. 30
-5. 30
+3. 10
+4. 20
+5. 15
+6. 30
 
 Actual Time (minutes):
-1. 90
-2. 10
-3. 30
-4. 30
-5. 30
+1. 15 
+2. not done
+3. 20
+4. 35
+5. 15
+6. 30
 */}
